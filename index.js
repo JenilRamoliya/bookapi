c62e522e55481470603f917da821be906e7b8054
JenilRamoliya <69188980+JenilRamoliya@users.noreply.github.com>
2021-01-29 10:00:54 +0530

    all update

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const route = require('./route');
+
+mongoose.connect("mongodb+srv://Jenil:Jenil@cluster0.dziyg.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+()=>{
+    const app = express();
+    app.use("/api",route);
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log("server started..!!");
+    })
+});
